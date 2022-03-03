47a77eed317482de5bcce898a2edfd4308a83c67
Julia Kordick <jkordick@frobese.de>
2022-03-03 12:38:36 +0100

    Added two more tasks

--- 04_schleifen.py
+++ 04_schleifen.py
@@ -12,12 +12,13 @@
 n = int(input('Bitte gib eine Zahl ein: '))
 
 for i in range(n + 1):
-    print(n * '*')
+    print(i * '*')
 
 # oder
 
 for i in range(n):
-    print((n + 1) * '*')
+    print((i + 1) * '*')
+
 
 # Schreibe ein Programm, das eine beliebige natürliche Zahl m einliest und dann m Zeilen
 # von Sternen in UMGEKEHRTER REIHENFOLGE ausgibt.
@@ -26,6 +27,38 @@ for i in range(n):
 # ***
 # **
 # * 
+
+m = int(input('Bitte gib eine Zahl ein: '))
+
+for i in range(m): 
+    print((m - i) * '*')
+
+
+# Schreibe ein Programm, das einen beliebigen String einliest und
+# diesen dann buchstabenweise ausgibt.
+# Beispiel:
+# Wort: Banane
+# B
+# a
+# n
+# a
+# n
+# e
+
+wort = input('Bitte gib ein Wort ein: ')
+
+for w in wort:
+    print(w)
+
+
+# Schreibe ein Programm, das einen beliebigen String einliest.
+# In diesem String sollen alle 'a' durch ein '*' ersetzt werden.
+# Beispiel: 
+# Wort: Banane
+# Ausgabe: B*n*ne
+
+
+
 
 
 
