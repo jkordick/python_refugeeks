7cb7422efa3fef1b2aa1dfd292db0bdd66b1ae6f
Julia Kordick <jkordick@frobese.de>
2022-03-07 08:58:52 +0100

    Added more tasks

--- 04_schleifen.py
+++ 04_schleifen.py
@@ -8,7 +8,7 @@
 # **
 # ***
 # ****
-
+"""""
 n = int(input('Bitte gib eine Zahl ein: '))
 
 for i in range(n + 1):
@@ -28,7 +28,7 @@ for i in range(n):
 # **
 # * 
 
-m = int(input('Bitte gib eine Zahl ein: '))
+ #m = int(input('Bitte gib eine Zahl ein: '))
 
 for i in range(m): 
     print((m - i) * '*')
@@ -45,10 +45,11 @@ for i in range(m):
 # n
 # e
 
-wort = input('Bitte gib ein Wort ein: ')
+# wort = input('Bitte gib ein Wort ein: ')
 
 for w in wort:
     print(w)
+
 
 
 # Schreibe ein Programm, das einen beliebigen String einliest.
@@ -57,26 +58,71 @@ for w in wort:
 # Wort: Banane
 # Ausgabe: B*n*ne
 
+neues_wort = input("Bitte gib ein Wort ein: ")
+#x = ''
+for w in neues_wort:
+    if w == 'a':
+        print('*', end = "")
+        #x = x + '*' # x += '*'
+    else:
+        print(w, end = "")
+        #x = x + w # x += w
+#print(x)    
+#print(neues_wort.replace('a', '*'))
 
 
 
+wort = input('Soll ich die Maschine Starten?')
 
+boolean = False
+if wort == "Ja":
+    boolean = True
+else:
+    boolean = False
 
+if boolean:
+    print ('ich starte die maschine')
+else: 
+    print('ich starte maschine NICHT')
 
-
-
-
-
-
-
-
-
-# Schreibe ein Programm, das eine beliebige natürliche Zahle einliest. 
+# Schreibe ein Programm, das eine beliebige natürliche Zahl einliest. 
 # Benutze eine for-Schleife, um alle ungeraden Zahlen, 
 # die zwischen 1 und der eingebenen Zahl liegen, auszugeben.
 # Beispiel:
 # Zahl: 10
 # Ausgabe: 1, 3, 5, 7, 9
+# oder: 
+# 1
+# 3
+# 5
+# 7
+# 9
+
+
+zahl = int(input("Bitte gib eine Zahl ein: "))
+
+for z in range(zahl + 1):
+    if z % 2 == 1:
+        print(z)
+
+"""
+
+# Schreibe ein Programm, das ein Rechteck aus Sternen ausgibt.
+# Dazu müssen die Höhe und die Breite des Rechtecks als natürliche Zahlen eingelesen werden.
+# Die Ausgabe soll beispielsweise so aussehen:
+
+# Die Höhe ist: 4
+# Die Breite ist: 3
+# ***
+# ***
+# ***
+# ***
+
+breite = int(input('Bitte gib eine Breite ein: '))
+hoehe = int(input('Bitte gib eine Höhe ein: '))
+
+for s in range(hoehe):
+    print(breite * '*')
 
 
 
