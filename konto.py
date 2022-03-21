8fc916793e93d6776a6a169185026941ccd7cdee
Julia Kordick <jkordick@frobese.de>
2022-03-21 11:41:00 +0100

    Added konto solution

--- konto.py
+++ konto.py
@@ -14,3 +14,58 @@
 # Für ein normales Konto und ein Festgeldkonto können nur Beträge abgehoben werden, die
 # kleiner als der Kontostand sind. Für ein Girokonto kann das Konto bis zu dem Betrag überzogen
 # werden, der durch Dispokredit festgelegt ist.
+
+class Konto: 
+    def __init__(self, kontoinhaber, kontonummer, kontostand):
+        self.kontoinhaber = kontoinhaber
+        self.kontonummer = kontonummer
+        self.kontostand = kontostand
+    
+    def __str__(self):
+        return str(self.kontostand) + ', ' + str(self.kontonummer) + ', ' + str(self.kontostand)
+    
+    def setKontoinhaber(self, neuer_kontoinhaber):
+        self.kontoinhaber = neuer_kontoinhaber
+    
+    # kontonummer private ODER bieten keinen Setter an ODER wir schreiben einen Dummy-Setter
+    def setKontonummer(self, neue_kontonummer):
+        return
+    
+    def einzahlen(self, betrag):
+        self.kontostand += betrag
+    
+    def abheben(self, betrag):
+        if betrag <= self.kontostand:
+            self.kontostand -= betrag
+        else: 
+            print("Die Auszahlung kann nicht durchgeführt werden.")
+
+# Kinderklassen bzw Subklassen erben von den Elternklassen Attribute UND Methoden/Funktionen
+class Festgeldkonto(Konto):
+     def __init__(self, kontoinhaber, kontonummer, kontostand):
+         Konto.__init__(self, kontoinhaber, kontonummer, kontostand)
+         self.zinssatz = 5
+    
+     def __str__(self):
+        return Konto.__str__(self) + ', ' + str(self.zinssatz)
+
+     def berechneZins(self):
+         self.kontostand = ((self.kontostand/100) * self.zinssatz) + self.kontostand
+
+class Girokonto(Konto):
+     def __init__(self, kontoinhaber, kontonummer, kontostand, dispokredit):
+         super().__init__(kontoinhaber, kontonummer, kontostand)
+         self.dispokredit = dispokredit
+
+     def __str__(self):
+        return super().__str__() + ', ' + str(self.dispokredit)
+    
+    # überladen bzw überschreiben eine Methode/Funktion
+     def abheben(self, betrag):
+        if betrag <= self.kontostand + self.dispokredit:
+            self.kontostand -= betrag
+        else: 
+            print("Die Auszahlung kann nicht durchgeführt werden.")
+
+
+    
