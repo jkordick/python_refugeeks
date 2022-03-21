acec402b69942bfde74799b2a4880578a0844b04
Julia Kordick <jkordick@frobese.de>
2022-03-21 09:27:56 +0100

    Specified konto task

--- konto.py
+++ konto.py
@@ -1,3 +1,4 @@
+# Teil 1
 # Schreibe eine Klasse Konto mit einen Kontoinhaber, einer Kontonummer und einem Kontostand
 # Der Name des Kontoinhabers soll geändert werden können, die Kontonummer aber nicht.
 #
@@ -6,3 +7,10 @@
 #
 # Schreibe eine Sub bzw. Kinderklasse Girokonto (im Gegensatz zu allen anderen Konten) um einen bestimmten Betrag - den
 # Dispokredit - überzogen werden kann.
+
+# Für alle drei Kontoarten sollen Einzahlungen möglich sein.
+
+# Für alle drei Kontoarten sollen Abhebungen möglich sein.
+# Für ein normales Konto und ein Festgeldkonto können nur Beträge abgehoben werden, die
+# kleiner als der Kontostand sind. Für ein Girokonto kann das Konto bis zu dem Betrag überzogen
+# werden, der durch Dispokredit festgelegt ist.
