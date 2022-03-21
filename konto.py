1622968e76645b9894f9bf89e017c5d10c968c3a
Julia Kordick <jkordick@frobese.de>
2022-03-21 09:25:56 +0100

    Added konto.py

---
+++ konto.py
@@ -0,0 +1,8 @@
+# Schreibe eine Klasse Konto mit einen Kontoinhaber, einer Kontonummer und einem Kontostand
+# Der Name des Kontoinhabers soll geändert werden können, die Kontonummer aber nicht.
+#
+# Schreibe eine Sub bzw. Kinderklasse Festgeldkonto, das kann mit einem gegebenen Zinssatz 
+# verzinst werden, der Zinssatz soll für alle Festgeldkonten gleich sein.
+#
+# Schreibe eine Sub bzw. Kinderklasse Girokonto (im Gegensatz zu allen anderen Konten) um einen bestimmten Betrag - den
+# Dispokredit - überzogen werden kann.
