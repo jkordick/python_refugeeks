933d48a365613cc04d8762d89e2caa50cbc81f99
Julia Kordick <jkordick@frobese.de>
2022-03-23 10:14:11 +0100

    added first try of account

--- passwordManager.py
+++ passwordManager.py
@@ -1,5 +1,38 @@
 # Wir schreiben einen Passwort-Manager
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Schreibe eine Klasse Account mit den drei Attributen
 # program - das Programm, zu dem der Account gehört
 # username - der Benutzername, der zum Zugang zu dem Programm verwendet wird
@@ -7,6 +40,68 @@
 # und zwei Methoden/Funktionen:
 # getProgramName() - die das zugehörige Program des Accounts zurück gibt
 # changePassword(password) - um das gespeicherte Passwort zu ändern
+
+class Account:
+    def __init__(self, program, username, password):
+        self.program = program
+        self.__username = username
+        self.__password = password
+    
+    def __str__(self):
+        return str(self.program) + ', ' + str(self.__username) + ', ' + str(self.__password)
+    
+    def getProgramName(self):
+        return self.program
+    
+    def __setPassword__(self, password):
+        self.__password = password
+    
+    def changePassword(self, password):
+        self.__setPassword__(password)
+    
+    def setProgram(self, program):
+        self.program = program
+    
+    def __getUsername__(self):
+        return self.__username
+    
+    def __setUsername__(self, username):
+        self.__username = username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Schreibe eine Klasse AdminAccount, die von Account erbt.
 # Überlege dir, wie man den AdminAccount besonders gegen die 
@@ -18,7 +113,7 @@
 # Schreibe eine Klasse PasswordManager mit zwei Attributen:
 # adminAccount - vom Typ AdminAccount
 # accounts - eine Liste mit Accounts
-# und x Methoden:
+# und 4 Methoden:
 # isPasswortValid(passwort) - gibt True oder False zurück
 # addAccount(account) - selbsterklärend
 # getAllAccounts() - gibt eine Liste aller Accounts zurück
