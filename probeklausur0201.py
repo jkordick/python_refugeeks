90f5bee529a055e8aebc47f00f39513afe162a54
Julia Kordick <jkordick@frobese.de>
2022-03-24 10:13:26 +0100

    Added points to pk0201

--- probeklausur0201.py
+++ probeklausur0201.py
@@ -1,4 +1,4 @@
-# 1) Erkläre in eigenen Worten die folgenden Begriffe:
+# 1) Erkläre in eigenen Worten die folgenden Begriffe: # 6
 # * Konstruktor # spezielle Methode/Funktion - initialisiert das Objekt, das aus der Klasse erstellt wird 
 # - ist notwendig, um ein ein Objekt aus einer Klasse erstellen zu können - instanziiert ein Objekt aus der Klasse
 # * Vererbung # wichtiges Konzept der objektorientierten Softwareentwicklung - 
@@ -10,24 +10,21 @@
 # 2) Schreibe eine Klasse Mensch mit den vier Attributen
 # geschlecht, alter, groesse und gewicht
 # Schreibe einen Konstuktor und eine String-Methode
-# (keine Get- und Set-Methoden)
+# (keine Get- und Set-Methoden) # 5
 
-from concurrent.futures import process
-
-
-class Mensch:
-    def __init__(self, geschlecht, alter, groesse, gewicht):
-        self.geschlecht = geschlecht
+class Mensch: # 1
+    def __init__(self, geschlecht, alter, groesse, gewicht): #1
+        self.geschlecht = geschlecht #1
         self.alter = alter
         self.groesse = groesse
         self.gewicht = gewicht
 
-    def __str__(self):
-        return str(self.geschlecht) + ', ' + str(self.alter) + ', ' + str(self.groesse) + ', ' + str(self.gewicht)
+    def __str__(self): #1
+        return str(self.geschlecht) + ', ' + str(self.alter) + ', ' + str(self.groesse) + ', ' + str(self.gewicht) #1
 
 # 3) Der folgende Programm-Abschnitt verursacht einen Fehler. 
 # Wieso tritt dieser Fehler auf und was musst du tun, um diesen
-# Fehler zu beheben?
+# Fehler zu beheben? # 4
 
 class Milkshake:
     def __init__(self, groesse, geschmack):
@@ -48,7 +45,7 @@ class Milkshake:
 # integer, string, float, boolean, none, list, tuple, dictionary
 
 # 5) Schreibe ein Programm. Schreibe nur Konstuktoren und die String-Methoden und 
-# zusätzlich geforderten fachlichen Methoden - keine Get- und Set-Methoden.
+# zusätzlich geforderten fachlichen Methoden - keine Get- und Set-Methoden. #27
 
 # Schreibe eine Klasse Obst mit drei Attributen:
 # * urspungsland - das Land, in dem das Obst angebaut und geerntet wurde
@@ -71,47 +68,56 @@ class Milkshake:
 # berechneVerkaufspreis, indem wir davon ausgehen, dass von
 # Bananen immer 10% Gewicht abgezogen werden, aufgrund der Schale
 
-class Obst:
-    def __init__(self, ursprungsland, preis, name):
-        self.ursprungsland = ursprungsland
+class Obst: #1
+    def __init__(self, ursprungsland, preis, name): #1
+        self.ursprungsland = ursprungsland #1
         self.preis = preis
         self.name = name
     
-    def __str__(self):
-        return str(self.ursprungsland) + str(self.preis) + str(self.name)
+    def __str__(self): #1
+        return str(self.ursprungsland) + str(self.preis) + str(self.name) #1
     
-    def aenderePreis(self, preis):
-        self.preis = preis
+    def aenderePreis(self, preis): #1
+        self.preis = preis #1
     
-    def berechneVerkaufspreis(self, gewichtInGramm): # Funktions-/Methodenkopf
+    def berechneVerkaufspreis(self, gewichtInGramm): # Funktions-/Methodenkopf #1
         preisProGramm = self.preis/1000 
         verkaufspreis = preisProGramm * gewichtInGramm
-        return verkaufspreis
+        return verkaufspreis #1
+
+# 9
 
 
-class Apfel(Obst):
-    def __init__(self, ursprungsland, preis, name, farbe): # def __init__(self, preis, farbe):
-        super().__init__("Deutschland", preis, "Apfel")
-        self.farbe = farbe
+class Apfel(Obst): #2
+    def __init__(self, ursprungsland, preis, name, farbe): # def __init__(self, preis, farbe): #1
+        super().__init__("Deutschland", preis, "Apfel") #2
+        self.farbe = farbe #1
     
-    def __str__(self):
-        return Obst.__str__(self) + str(self.farbe)
+    def __str__(self): #1
+        return Obst.__str__(self) + str(self.farbe) #1
+
+# 8
 
 
-class Banane(Obst):
-    def __init__(self, ursprungsland, preis, name, reifegrad): # def __init__(self, preis, reifegrad):
-        super().__init__("Brasilien", preis, "Banane")
-        self.reifegrad = reifegrad
+class Banane(Obst): #2
+    def __init__(self, ursprungsland, preis, name, reifegrad): # def __init__(self, preis, reifegrad): #1
+        super().__init__("Brasilien", preis, "Banane") #2
+        self.reifegrad = reifegrad #1
 
-    def __str__(self):
-        return Obst.__str__(self) + str(self.reifegrad)
+    def __str__(self): #1
+        return Obst.__str__(self) + str(self.reifegrad) #1
 
-    def berechneVerkaufspreis(self, gewichtInGramm):
+    def berechneVerkaufspreis(self, gewichtInGramm): #1
         gewichtDer10Prozent = gewichtInGramm/100 * 10
         endGewicht = gewichtInGramm - gewichtDer10Prozent
         preisProGramm = self.preis/1000 
         verkaufspreis = preisProGramm * endGewicht
-        return verkaufspreis
+        return verkaufspreis #1
+
+# 10
+
+
+# 42
 
 
   
