54d3e1700c94e91d48b75f8706f8a2363b37249f
Julia Kordick <jkordick@frobese.de>
2022-04-11 12:14:24 +0200

    Added api-call homework

--- python_3/api_call.py
+++ python_3/api_call.py
@@ -1,13 +1,11 @@
 import requests
 
-response = requests.get("https://swapi.dev/api/people/1")
+#API: application programming interface -> Anwendungsschnittstelle oder Programmierschnittstelle
 
-#API: application programming interface -> Anwendungsschnittstelle
+#print(response.text) # String
 
-print('Status Code:')
-print(response.status_code)
-print(response.text) # String
-print(response.json()) # Dictionary
+# Internet iPhone von Julia
+# PW: 11223344
 
 # Schreibe eine Funktion, die einen request durchführt, um mehrere people zu erhalten.
 # Als request könnt ihr verwenden: https://swapi.dev/api/people/
@@ -17,9 +15,40 @@ print(response.json()) # Dictionary
 # Anakin Skywalker
 # ...
 
+# Schreibe eine eigene Exception, um den Request sicher durchführen zu können.
+# Den Request in ein try/except schreiben.
+
+# Hausaufgabe (freiwillig):
+# Schreibe eine Klasse Person, um die people in einem Objekt zu speichern.
+# Die Klasse Person soll die vier Attribute name, height/groesse, mass/gewicht und haarfarbe/haircolor haben.
+# Schreibe einen Konstruktor und eine String-Methode.
+# Nutze zur Erstellung der Personen-Objekte eine Schleife.
+
+class RequestException(Exception):
+    def __str__(self):
+        return "Beim Request ist ein Fehler aufgetreten."
+
+def getAllNames():
+    try:
+        response = requests.get("https://swapi.dev/api/people/")
+    except:
+        print(RequestException())
+    dict = response.json()
+    listOfDicts = dict['results']
+
+    for l in listOfDicts:
+        print(l['name'])
+
+
+
+getAllNames()
+
+
+# HTTP Status Codes:
 # 2xx - alles okay 
 # 4xx - Fehler auf Client-Seite
 # 5xx - Fehler auf Server-Seite
+# Alle weiteren hier: https://httpstat.us/
 
 # zum Ausprobieren:
 # print(response.text)
