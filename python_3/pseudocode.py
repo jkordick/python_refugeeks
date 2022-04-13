c4bd5e0cc528dd2111e9064a591ab7b14e8865ca
Julia Kordick <jkordick@frobese.de>
2022-04-13 11:36:37 +0200

    Added more pseudocode solutions

--- python_3/pseudocode.py
+++ python_3/pseudocode.py
@@ -19,9 +19,8 @@ noteAuswerten(3.0)
 #   Name zur Liste Namen hinzufügen
 #   Zähler + 1
 # print Namen
-
-def namenListeErstellen():
-    zaehler = 0
+"""
+def namenListeErstellen(zaehler):
     namen = []
     while zaehler <= 10:
         name = input("Bitte gib einen Namen ein")
@@ -29,16 +28,40 @@ def namenListeErstellen():
         zaehler += 1 # zaehler = zaehler + 1
     print(namen)
 
-namenListeErstellen()
+namenListeErstellen(0)
 
-
+"""
 # 3. Aufgabe
 # namenHinzufuegen(liste)
-#   setze einen Zähler auf 0
+#   setze einen Zähler auf 0 # falle
 #   so lange Zähler <= 10
 #       input Name
 #       Name zur Liste hinzufügen            
 #       Zähler + 1
 #       namenHinzufuegen(liste)
 
+# zähler benutzen den als abbruchbedingung
+# oder benutzen die länge der liste als abbruchbedingung
+
 # Aufruf von namenHinzufügen mit einer leeren Liste
+
+def namenHinzufügen(liste):
+    if len(liste) > 3: # Abbruchbedingung
+        print(liste)
+        return
+    else: 
+        name = input("Bitte gib einen Namen ein: ")
+        liste.append(name)
+        namenHinzufügen(liste)
+namenHinzufügen([])
+
+
+def namenHinzufügen2(liste, zaehler):
+        if zaehler > 3: # Abbruchbedingung
+            print(liste)
+            return
+        else:
+            name = input("Bitte gib einen Namen ein")
+            liste.append(name)
+            namenHinzufügen2(liste, zaehler + 1)
+namenHinzufügen2([], 0)
