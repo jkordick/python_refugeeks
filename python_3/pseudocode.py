3e27a02d94c6da0a0121a6785f0f6163cac9e07c
Julia Kordick <jkordick@frobese.de>
2022-04-13 09:42:03 +0200

    Added pseudocode

---
+++ python_3/pseudocode.py
@@ -0,0 +1,23 @@
+# 1. Aufgabe
+# wenn die Note einer Schülerin kleiner oder gleich 4.0 ist
+# dann print Bestanden
+# sonst print Nicht Bestanden
+
+def noteAuswerten(note):
+    if note <= 4.0:
+        print("Bestanden")
+    else:
+        print("Nicht Bestanden")
+
+noteAuswerten(3.0)
+
+# 2. Aufgabe
+# setze einen Zähler auf 0
+# erstelle eine leere Liste Namen
+# so lange der Zähler kleiner oder gleich 10 ist
+#   input Name
+#   Name zur Liste Namen hinzufügen
+#   Zähler + 1
+# print Namen
+
+# 3. Aufgabe
