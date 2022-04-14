a31fdb021b454c967cabc3a75ce9002536de54c9
Julia Kordick <jkordick@frobese.de>
2022-04-14 09:20:02 +0200

    Added merge_sort solution

--- python_3/merge_sort.py
+++ python_3/merge_sort.py
@@ -1,20 +1,46 @@
+def merge(mainList, leftList, rightList):
+    indexLeft = indexRight = indexMain = 0
+
+    while indexLeft < len(leftList) and indexRight < len(rightList):
+        if leftList[indexLeft] <= rightList[indexRight]:
+            mainList[indexMain] = leftList[indexLeft]                
+            indexLeft += 1
+        else:
+            mainList[indexMain] = rightList[indexRight]
+            indexRight += 1
+        indexMain += 1
+
+    while indexLeft < len(leftList):
+            mainList[indexMain] = leftList[indexLeft]
+            indexLeft += 1
+            indexMain += 1
+
+    while indexRight < len(rightList):
+            mainList[indexMain] = rightList[indexRight]
+            indexRight += 1
+            indexMain += 1
+
+    print("Aktuelle sortierte Liste:", mainList)
+
+
 def merge_sort(liste):
     if len(liste) < 2:  # Abbruchbedingung, um die Rekursion beenden zu können
         return liste
     mitte = len(liste) // 2
 
-    print("Aktuelle Liste: ", liste)
+    #print("Aktuelle Liste: ", liste)
 
     linkeListe = liste[:mitte]
     rechteListe = liste[mitte:]
 
-    print("Aktuelle linke Liste: ", linkeListe)
-    print("Aktuelle rechte Liste", rechteListe)
+    #print("Aktuelle linke Liste: ", linkeListe)
+    #print("Aktuelle rechte Liste", rechteListe)
 
     merge_sort(linkeListe)
     merge_sort(rechteListe)
 
-    #return merge(list, leftList, rightList)
+    return merge(liste, linkeListe, rechteListe)
+
 
 merge_sort([54,26,93,17,77,31,44,55,20])
 
