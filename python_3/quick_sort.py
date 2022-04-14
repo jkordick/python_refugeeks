166437e2fcfeb12f6168c5a79dcc450dac9e41d9
Julia Kordick <jkordick@frobese.de>
2022-04-14 09:50:13 +0200

    Added quick_sort

---
+++ python_3/quick_sort.py
@@ -0,0 +1,24 @@
+def quick_sort(start, end, array):
+    if (start < end):
+        p = partition(start, end, array)
+
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
+
+def partition(start, end, array):
+    pivot_index = start
+    pivot = array[pivot_index]
+
+    while start < end:
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+
+        while array[end] > pivot:
+            end -= 1
+
+        if (start < end):
+            array[start], array[end] = array[end], array[start]
+
+        array[end], array[pivot_index] = array[pivot_index], array[end]
+
+    return end
