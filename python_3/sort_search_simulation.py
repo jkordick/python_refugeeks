821d55c014fefb7f9d1d494051a17cd42f2a809b
Julia Kordick <jkordick@frobese.de>
2022-04-14 10:36:04 +0200

    Added more tasks

--- python_3/sort_search_simulation.py
+++ python_3/sort_search_simulation.py
@@ -12,6 +12,12 @@
 # 1.Schritt: Einzelne Algorithmen. Nicht vergessen: Wenn wir suchen, müssen wir das Suchelement 
 # eingeben können.
 # 2. Schritt: Kombinationen von Algorithmen. Bsp.: 1. Quicksort 2. Binäre-Suche
+# 3. Schritt: Achtung, eigene Recherche :) Finde heraus, wie die Listen, die wir für die Suche/das Sortieren nutzen
+# randomisiert generiert werden können. Erweitere die Simulation um folgende Abfrage:
+# Wie viele Elemente soll die Liste enthalten?
+# Eingabe: 1000
+# Der BubbleSort wurde durchgeführt mit einer Liste mit 1000 Elementen.
+# Die Laufzeit war: 0.43232 Sekunden
 
 # Wie machen wir die Interaktion Mensch <-> Computer?
 # Wie entscheide ich nach der Eingabe, welchen Algorithmus bzw. welche Funktion ich aufrufen muss?
