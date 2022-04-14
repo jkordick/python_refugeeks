6a753853a65c6e4f312118ede39a13322228824a
Julia Kordick <jkordick@frobese.de>
2022-04-14 09:47:53 +0200

    Added task

---
+++ python_3/sort_search_simulation.py
@@ -0,0 +1,18 @@
+# Schreibe ein Programm, das alle Sortier und Suchalgorithmen, die wir
+# kennen gelernt haben, enthält.
+# Diese müssen alle, in einzelnen Funktionen sein, damit wir diese aufrufen können.
+# Steuert über einen input, welchen Such und/oder Sortieralgorithmus, ihr verwenden möchtest.
+# Für jeden Simulationsdurchlauf eine Zeitmessung machen.
+# Bsp.:
+# Welcher Algorithmus soll verwendet werden?
+# Eingabe: Bubble
+# BubbleSort wurde durchgeführt mit folgender Liste: [xxx]
+# Die Laufzeit war: 0.32323232 Sekunden
+#
+# 1.Schritt: Einzelne Algorithmen. Nicht vergessen: Wenn wir suchen, müssen wir das Suchelement 
+# eingeben können.
+# 2. Schritt: Kombinationen von Algorithmen. Bsp.: 1. Quicksort 2. Binäre-Suche
+
+# Wie machen wir die Interaktion Mensch <-> Computer?
+# Wie entscheide ich nach der Eingabe, welchen Algorithmus bzw. welche Funktion ich aufrufen muss?
+# Wie und wo messe ich die Zeit, damit es immer für jede Kombination funktioniert?
