208dd56b5ac7144a92a1ff9e47388112a3f5392b
Julia Kordick <jkordick@frobese.de>
2022-04-19 09:41:26 +0200

    Added first task

---
+++ python_3/getting_started_with_num.py
@@ -0,0 +1,12 @@
+# Mac: >>> pip3 install numpy
+# Windows: >>> python3 -m pip install numpy
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+# Gegeben ist folgendes n-dimensionales Array: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+# Lass dir die Nummer 8 ausgeben. 
+# Lass dir die Nummer 2 ausgeben.
+# Lass dir das letzte Element der zweiten Dimension ausgeben. # Recherche :)
