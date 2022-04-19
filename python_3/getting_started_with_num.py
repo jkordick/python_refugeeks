bb450f0943bc51249f669cd171b0e968cd79ce93
Julia Kordick <jkordick@frobese.de>
2022-04-19 09:57:47 +0200

    Aded task three

--- python_3/getting_started_with_num.py
+++ python_3/getting_started_with_num.py
@@ -3,10 +3,23 @@
 
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
 
 # Gegeben ist folgendes n-dimensionales Array: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
 # Lass dir die Nummer 8 ausgeben. 
 # Lass dir die Nummer 2 ausgeben.
 # Lass dir das letzte Element der zweiten Dimension ausgeben. # Recherche :)
+
+# arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+# print(arr[1, 2])
+# print(arr[0, 1])
+# print(arr[1, -1])
+
+# Gegeben ist folgendes n-dimensionales Array: [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+# Lass die die Nummer 7 ausgeben.
+# Welche Nummer wird mit dem folgenden Aufruf arr[1, 0, 2] ausgegeben?
+# Lass dir die Nummer 11 ausgeben.
+# Welche Nummer wird mit dem folgenden Aufruf arr[0, 0, 1] ausgegeben?
+# Addiere die Werte an den Indexen [0, 1, 0] und [1, 1, 0] - welches Ergebnis kommt dabei heraus?
+
