ee3ddd5edaee70d2a1733597066769751a4df3f6
Julia Kordick <jkordick@frobese.de>
2022-04-19 10:25:47 +0200

    Added first slicing task

--- python_3/getting_started_with_num.py
+++ python_3/getting_started_with_num.py
@@ -23,16 +23,29 @@ import numpy as np
 # Welche Nummer wird mit dem folgenden Aufruf arr[0, 0, 1] ausgegeben?
 # Addiere die Werte an den Indexen [0, 1, 0] und [1, 1, 0] - welches Ergebnis kommt dabei heraus?
 
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-print(arr[1, 0, 0])
-print(arr[1, 0, 2])
-print(arr[1, 1, 1])
-print(arr[0, 0, 1])
-print(arr[0, 1, 0] + arr[1, 1, 0]) # 4 + 10
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# print(arr[1, 0, 0])
+# print(arr[1, 0, 2])
+# print(arr[1, 1, 1])
+# print(arr[0, 0, 1])
+# print(arr[0, 1, 0] + arr[1, 1, 0]) # 4 + 10
 
 # Gegeben ist folgendes n-dimensionales Array: [1, 2, 3, 4]
 # Lass dir die Nummer 2 ausgeben.
 # Lass dir die Nummer 1 ausgeben.
 # Addiere die Werte an den Indexen [1] und [2] - welches Ergebnis kommt dabei heraus?
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr[1])
+# print(arr[0])
+# print(arr[1] + arr[2])
+
+# Wiederholung Slicing
+# Erinnerung: Das Ergebnis enthält den Startindex, jedoch nicht den Endindex. 
+
+# Gegeben ist folgendes n-dimensionales Array: [1, 2, 3, 4, 5, 6, 7]
+# Wie muss ich Slicing anwenden, um das Ergebnis [2, 3, 4] zu erhalten?
+# Wie muss ich Slicing anwenden, um das Ergebnis [6, 7] zu erhalten?
+# Beispiel negatives Slicing: [-5:-1] - was ist das Ergebnis?
 
 
